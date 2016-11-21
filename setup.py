94ead6857e7e9b127be95b429bebbbc9b21e0037
Joseph E. Curtis <madscatt@gmail.com>
2016-11-20 22:34:52 -0500

    partial flake cleanup of setup

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@
     This is free software, and you are welcome to redistribute it under certain
     conditions; see http://www.gnu.org/licenses/gpl-3.0.html for details.
 '''
-import os,sys
-from distutils      import sysconfig
+import os
 #import setuptools
 from numpy.distutils.core import Extension, setup
 #       SETUP
@@ -26,41 +25,39 @@ from numpy.distutils.core import Extension, setup
 
 import numpy
 
-try:
-    numpy_include = numpy.get_include()
-except AttributeError:
-    numpy_include = numpy.get_numpy_include()
+NUMPY_INCLUDE = numpy.get_include()
 
 def read(fname):
+    ''' method to read a file name '''
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='sasmol',
-	version='2.0',
-	author='Joseph E. Curtis',
-	author_email='joseph.curtis@nist.gov',
-	license='GPL 3',
-	url='https://github.com/madscatt/sasmol',
-	platforms='Linux, Mac OS X',
-	description=("A library of methods to write software for molecular systems"),
-	long_description=read('README.md'),	
-	classifiers=["Development Status :: 4 - Beta",
-		"License :: OSI Approved :: GNU Public License 3",
-		"Intended Audience :: Science/Research",
-		"Natural Language :: English",
-		"Operating System :: Linux :: MacOS :: MacOS X",
-		"Programming Language :: Python :: C :: Fortran",
-		"Topic :: Scientific/Engineering :: Chemistry :: Physics"],
+      version='2.0',
+      author='Joseph E. Curtis',
+      author_email='joseph.curtis@nist.gov',
+      license='GPL 3',
+      url='https://github.com/madscatt/sasmol',
+      platforms='Linux, Mac OS X',
+      description=("A library of methods to write software for molecular systems"),
+      long_description=read('README.md'),
+      classifiers=["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: GNU Public License 3",
+                   "Intended Audience :: Science/Research",
+                   "Natural Language :: English",
+                   "Operating System :: Linux :: MacOS :: MacOS X",
+                   "Programming Language :: Python :: C :: Fortran",
+                   "Topic :: Scientific/Engineering :: Chemistry :: Physics"],
 
-	package_dir={'sasmol':os.path.join('src','python')},
+      package_dir={'sasmol':os.path.join('src','python')},
 
-    packages=['sasmol','sasmol.test_sasmol','sasmol.test_sasmol.utilities','sasmol.test_sasmol.manual_tests','sasmol.test_sasmol.data','sasmol.test_sasmol.data.pdb_common','sasmol.test_sasmol.data.dcd_common','sasmol.test_sasmol.data.sasmol','sasmol.test_sasmol.data.sasmol.calculate','sasmol.test_sasmol.data.sasmol.file_io','sasmol.test_sasmol.data.sasmol.file_io.test-results','sasmol.test_sasmol.data.sasmol.linear_algebra','sasmol.test_sasmol.data.sasmol.system','sasmol.test_sasmol.data.sasmol.operate','sasmol.test_sasmol.data.sasmol.properties','sasmol.test_sasmol.test_calculate','sasmol.test_sasmol.test_file_io','sasmol.test_sasmol.test_linear_algebra','sasmol.test_sasmol.test_operate','sasmol.test_sasmol.test_properties','sasmol.test_sasmol.test_subset','sasmol.extensions','sasmol.extensions.dcdio','sasmol.extensions.view','sasmol.extensions.mask','sasmol.extensions.matrix_math'],
-	
+      packages=['sasmol','sasmol.test_sasmol','sasmol.test_sasmol.utilities','sasmol.test_sasmol.manual_tests','sasmol.test_sasmol.data','sasmol.test_sasmol.data.pdb_common','sasmol.test_sasmol.data.dcd_common','sasmol.test_sasmol.data.sasmol','sasmol.test_sasmol.data.sasmol.calculate','sasmol.test_sasmol.data.sasmol.file_io','sasmol.test_sasmol.data.sasmol.file_io.test-results','sasmol.test_sasmol.data.sasmol.linear_algebra','sasmol.test_sasmol.data.sasmol.system','sasmol.test_sasmol.data.sasmol.operate','sasmol.test_sasmol.data.sasmol.properties','sasmol.test_sasmol.test_calculate','sasmol.test_sasmol.test_file_io','sasmol.test_sasmol.test_linear_algebra','sasmol.test_sasmol.test_operate','sasmol.test_sasmol.test_properties','sasmol.test_sasmol.test_subset','sasmol.extensions','sasmol.extensions.dcdio','sasmol.extensions.view','sasmol.extensions.mask','sasmol.extensions.matrix_math'],
+
     ext_modules=[
-	Extension('sasmol._dcdio',[os.path.join('src','python','extensions','dcdio','dcdio.i'),os.path.join('src','python','extensions','dcdio','dcdio.c')],include_dirs=[numpy_include]),
-	Extension('sasmol._view_vmd',[os.path.join('src','python','extensions','view','view_vmd.i'),os.path.join('src','python','extensions','view','view_vmd.c'),os.path.join('src','python','extensions','view','imd.c'),os.path.join('src','python','extensions','view','vmdsock.c')],include_dirs=[numpy_include]),
-	Extension('sasmol._mask',[os.path.join('src','python','extensions','mask','mask.i'),os.path.join('src','python','extensions','mask','mask.c')],include_dirs=[numpy_include]),
-	Extension('sasmol.foverlap',[os.path.join('src','python','extensions','overlap','foverlap.f')],include_dirs=[numpy_include]),
-	Extension('sasmol.matrix_math',[os.path.join('src','python','extensions','matrix_math','matrix_math.f')],include_dirs=[numpy_include])],
+    Extension('sasmol._dcdio',[os.path.join('src','python','extensions','dcdio','dcdio.i'),os.path.join('src','python','extensions','dcdio','dcdio.c')],include_dirs=[NUMPY_INCLUDE]),
+    Extension('sasmol._view_vmd',[os.path.join('src','python','extensions','view','view_vmd.i'),os.path.join('src','python','extensions','view','view_vmd.c'),os.path.join('src','python','extensions','view','imd.c'),os.path.join('src','python','extensions','view','vmdsock.c')],include_dirs=[NUMPY_INCLUDE]),
+    Extension('sasmol._mask',[os.path.join('src','python','extensions','mask','mask.i'),os.path.join('src','python','extensions','mask','mask.c')],include_dirs=[NUMPY_INCLUDE]),
+    Extension('sasmol.foverlap',[os.path.join('src','python','extensions','overlap','foverlap.f')],include_dirs=[NUMPY_INCLUDE]),
+    Extension('sasmol.matrix_math',[os.path.join('src','python','extensions','matrix_math','matrix_math.f')],include_dirs=[NUMPY_INCLUDE])],
 
     data_files = [ 
         ( os.path.join('sasmol','test_sasmol','manual_tests') , [os.path.join('src','python','test_sasmol','manual_tests','hiv1_gag.pdb')]),
@@ -89,8 +86,7 @@ setup(name='sasmol',
         ( os.path.join('sasmol','test_sasmol','data','sasmol','calculate') , [os.path.join('src','python','test_sasmol','data','sasmol','calculate','1ATN.pdb')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','calculate') , [os.path.join('src','python','test_sasmol','data','sasmol','calculate','1CRN-rot.pdb')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','calculate') , [os.path.join('src','python','test_sasmol','data','sasmol','calculate','1CRN-rot-shift.pdb')]),
-    
-    
+
         ( os.path.join('sasmol','test_sasmol','data','sasmol','properties') , [os.path.join('src','python','test_sasmol','data','sasmol','properties','Catoms.txt')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','properties') , [os.path.join('src','python','test_sasmol','data','sasmol','properties','ConflictAtoms.txt')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','properties') , [os.path.join('src','python','test_sasmol','data','sasmol','properties','Hatoms.txt')]),
@@ -108,10 +104,10 @@ setup(name='sasmol',
         ( os.path.join('sasmol','test_sasmol','data','sasmol','operate') , [os.path.join('src','python','test_sasmol','data','sasmol','operate','1CRN-rot-sub.pdb')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','operate') , [os.path.join('src','python','test_sasmol','data','sasmol','operate','1CRN-rot.pdb')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','operate') , [os.path.join('src','python','test_sasmol','data','sasmol','operate','1CRN-sub.pdb')]),
-    
+
         ( os.path.join('sasmol','test_sasmol','data','sasmol','linear_algebra') , [os.path.join('src','python','test_sasmol','data','sasmol','linear_algebra','1CRN-rot-shift.pdb')]),
         ( os.path.join('sasmol','test_sasmol','data','sasmol','linear_algebra') , [os.path.join('src','python','test_sasmol','data','sasmol','linear_algebra','1CRN-rot.pdb')]),
-        
+
         ( os.path.join('sasmol','test_sasmol','data','sasmol','sasmol') , [os.path.join('src','python','test_sasmol','data','sasmol','sasmol','1CRN-3frames.pdb')]),
 
         ( os.path.join('sasmol','test_sasmol','data','sasmol','file_io') , [os.path.join('src','python','test_sasmol','data','sasmol','file_io','1AA-NoEND.pdb')]),
@@ -136,6 +132,5 @@ setup(name='sasmol',
         ( os.path.join('sasmol','test_sasmol','data','sasmol','file_io') , [os.path.join('src','python','test_sasmol','data','sasmol','file_io','new_package_rna.pdb')]) #,
 
                    ]
-	)
-
+    )
 
